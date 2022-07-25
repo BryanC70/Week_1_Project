cf8c5e403ef3863386620d73c9b995c046ab00a7
Bryan Camarillo <bryan07@mail.fresnostate.edu>
2022-07-25 16:53:34 +0000

    tests

---
+++ .github/workflows/test.py
@@ -0,0 +1,6 @@
+import unittest
+from main import get_channel_statistics
+
+def test_channel(unittest.TestCase):
+  def test_stats(self):
+    self.assertIsInstance(data, get_channel_statistics, msg= None))
